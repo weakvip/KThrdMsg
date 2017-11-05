9566a5fe35fcbfc8e9c951967b03ebaee1065be5
weakvip <weakvip@163.com>
2017-11-05 18:01:07 +0800

    Add files via upload
    change parameter

--- KThrdMsgTest/KThrdMsgTest.cpp
+++ KThrdMsgTest/KThrdMsgTest.cpp
@@ -1,85 +1,75 @@
 // KThreadMsgTest.cpp : Defines the entry point for the console application.
 //
-
-#include <Inc/ThrdMsg/IThrdMsg.h>
+#include <assert.h>
 #include <string>
 #include <iostream>
 #include <chrono>
-
-#ifdef WINDOWS
-#include <Windows.h>
-#endif
+#include <thread>
+#include <Inc/KThrdMsg/IThrdMsg.h>
 
 int main()
 {
-	PThrdMsgMgr pMgr = CreateThrdMsgMgr();
+	thrd_msg_mgr_p pMgr = CreateThrdMsgMgr();
 	if (!pMgr) {
 		std::cout << "create msgmgr error" << std::endl;
-		return -1;
 	}
-
-	for (;;) {
-
-		int cnt = 0;
-
+	else {
 		std::chrono::time_point<std::chrono::system_clock> p1, p2;
-		p1 = std::chrono::system_clock::now();
-
 		const int cnBufSize = 20;
-		const int cnLoopCnt = 20;
+		const int cnLoopCnt = 10000;
 
-		for (int i = 0; i < cnLoopCnt; ++i) {
-			char* pbuf = (char*)malloc(cnBufSize);
-			snprintf(pbuf, cnBufSize, "%s", "hello world");
-			pbuf[cnBufSize - 1] = '\0';
-			PThrdMsg pMsg = GetThrdMsg(0, 0, 0, 0, pbuf, cnBufSize);
-			if (pMsg) {
-				int n = AddMsg(pMgr, pMsg, sizeof(pMsg));
+		for (;;) {
+			int cnt = 0;
+			p1 = std::chrono::system_clock::now();
+
+			for (int i = 0; i < cnLoopCnt; ++i) {
+				char* buf = (char*)malloc(cnBufSize);
+				if (buf) {
+					snprintf(buf, cnBufSize, "%s", "hello world");
+					*(buf + cnBufSize - 1) = '\0';
+				}
+				int n = PushMsg(pMgr, buf, cnBufSize);
 				assert(0 == n);
 				++cnt;
 			}
+
+			std::cout << "push cnt:" << cnt << std::endl;
+			cnt = 0;
+
+			for (;;) {
+				char* buf = 0;
+				int size = 0;
+				int num = 0;
+				if (PopMsg(pMgr, 0, (void**)&buf, &size)) {
+					break;
+				}
+				if (buf && cnBufSize==size) {
+					if (1 == ++cnt) {
+						std::cout << "msg[" << buf << "]" << std::endl;
+					}					
+					free(buf);
+				}
+			};
+
+			std::cout << "pop cnt:" << cnt << std::endl;
+
+			p2 = std::chrono::system_clock::now();
+
+			std::cout << "cost:" << (p2.time_since_epoch().count() - p1.time_since_epoch().count()) << std::endl;
+
+			std::this_thread::sleep_for(std::chrono::seconds(2));
 		}
 
-		std::cout << "add cnt:" << cnt << std::endl;
-		cnt = 0;
-
-		for (;;) {
-			PThrdMsg pMsgR = (PThrdMsg)GetMsg(pMgr, 0);
-			if (pMsgR) {
-				++cnt;
-				std::cout << "msg[" << (char*)GetMsgData(pMsgR) << "]" << std::endl;
-				FreeThrdMsg(pMsgR);
-			}
-			else {
-				break;
-			}
-		};
-
-		std::cout << "get cnt:" << cnt << std::endl;
-
-		p2 = std::chrono::system_clock::now();
-
-		std::cout << "last:" << (p2.time_since_epoch().count() - p1.time_since_epoch().count()) << std::endl;
-
-#ifdef WINDOWS
-
-		Sleep(1000*2);
-#else
-		usleep(1000 * 1000*2);
-#endif
-
+		DestroyThrdMsgMgr(&pMgr);
 	}
 
-	DestroyThrdMsgMgr(pMgr);	
-
 	std::cout << "game over" << std::endl;
-
-#ifdef WINDOWS
+#ifdef _WIN32
 	system("pause");
 #else
-	usleep(1000 * 1000*2);
+	std::cout << "press any key to continue...\n" << std::endl;
+	getchar();
 #endif
 
     return 0;
 }
-
